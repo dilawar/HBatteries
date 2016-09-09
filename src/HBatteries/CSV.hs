a840182be27dbc4ff3c378b766b89ab69833c81a
Dilawar Singh <dilawars@ncbs.res.in>
2016-09-09 14:48:52 +0530

    readCSV is all alive.

--- src/HBatteries/CSV.hs
+++ src/HBatteries/CSV.hs
@@ -1,6 +1,7 @@
 module HBatteries.CSV 
     ( 
     readCSV
+    , getColumn
     ) where
 
 {-# LANGUAGE ScopedTypeVariables #-}
@@ -9,42 +10,36 @@ import qualified Data.Csv.Parser as CP
 import Data.Maybe
 import qualified Data.Vector as V
 import qualified Data.ByteString.Lazy as BS
-import Data.ByteString.Char8 (pack)
+import qualified Data.ByteString.Char8 as C
 import Data.ByteString.Lex.Fractional (readDecimal)
 import qualified Data.List as L
 import Data.Attoparsec.ByteString.Lazy (parse, maybeResult)
-import qualified Data.HashMap.Lazy as HL 
+import qualified Data.HashMap.Strict as HL 
+import Data.ByteString.Lazy.Internal ( packChars )
+import qualified Data.ByteString.Internal as BS (c2w, w2c)
 
 type Vec = V.Vector Double
 
 instance C.FromRecord Double
 
-decodeOpt = C.DecodeOptions 32 -- 32 is space
-
-(low, high, thres) = (0, 4, 1) 
+{-toWord8 = fromIntegral . fromEnum-}
 
 -- Get a given column.
-{-getColumn :: String ->  -> Vec-}
 getColumn columnName (header, res) = V.map (\x ->  fst $ fromJust $ readDecimal x) $
-    V.map (\x -> fromJust $ HL.lookup (pack columnName) x ) res
+    V.map (\x -> x HL.! (C.pack columnName) ) res
 
-find_transitions vec = find_transitions' 1 [(0, V.head vec == high)] (V.tail vec)
-  where 
-    find_transitions' i ss v 
-        | V.null v = ss
-        | highToLow (head ss) (V.head v) = find_transitions' (i+1) ((i,False):ss) (V.tail v)
-        | lowToHigh (head ss) (V.head v) = find_transitions' (i+1) ((i,True):ss) (V.tail v)
-        | otherwise = find_transitions' (i+1) ss (V.tail v)
-
-lowToHigh (i,False) val = if val >= (high - thres) then True else False
-lowToHigh _ _ = False
-highToLow (i,True) val = if val <= (low + thres) then True else False
-highToLow _ _ = False
-
+{-resdCSVWithDouble :: String -> Char -> IO( ( C.Header, V.Vector Double) )-}
 
 -- ! Read a csv file with given delimiter .
 readCSV :: String -> Char -> IO ((C.Header,  V.Vector C.NamedRecord))
-readCSV filepath delimeter = BS.readFile filepath 
-    >>= \text -> return $ fromJust $ maybeResult $ parse (CP.csvWithHeader decodeOpt) text 
+readCSV filepath delimeter = do 
+    text <- BS.readFile filepath
+    let lines = filter (not . BS.null) $ BS.split (BS.c2w '\n') text
+    -- Ignore all lines starting with '#'
+    let filtered = filter (\l -> BS.index l 0 /= BS.c2w '#' ) lines
+    let t = BS.intercalate (packChars "\n") filtered
+    return . fromJust . maybeResult $ parse (CP.csvWithHeader decodeOpt) t 
   where 
-    decodeOpt = C.DecodeOptions (fromIntegral $ fromEnum delimeter)
+    decodeOpt = C.DecodeOptions (BS.c2w delimeter)
+
+
